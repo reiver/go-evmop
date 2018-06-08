ad003ecf1b994f5132e1713d3f464c4006c38495
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:00:26 -0700

    SUB OpCode

---
+++ sub.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Sub represents the "SUB" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Sub{}.WriteTo(w)
+type Sub struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (Sub) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_SUB)
+}
