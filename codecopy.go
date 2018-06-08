57aace7c6257402376d1b63a74ea7bf992ca34a7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 15:35:39 -0700

    CODECOPY OpCode

---
+++ codecopy.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// CodeCopy represents the "CODECOPY" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.CodeCopy{}.WriteTo(w)
+type CodeCopy struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (CodeCopy) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_CODECOPY)
+}
