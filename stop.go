e801b55711c04a8309f5f1172bf87935b8b21a0f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 12:24:21 -0700

    STOP OpCode

---
+++ stop.go
@@ -0,0 +1,46 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Stop represents the "STOP" Ethereum Virtual Machine (EVM) OpCode.
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Stop{}.WriteTo(w)
+type Stop struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (Stop) WriteTo(writer io.Writer) (int64, error) {
+
+	const opcode = code_STOP
+
+	if nil == writer {
+		return 0, errNilWriter
+	}
+
+	var n64 int64
+
+	{
+		var b [1]byte = [1]byte{opcode}
+		p := b[:]
+
+		n, err := writer.Write(p)
+		if nil != err {
+			return n64, err
+		}
+		if 0 > n {
+			return n64, errNegativeNumberOfBytesWritten
+		}
+		n64 += int64(n)
+	}
+
+	return n64, nil
+}
