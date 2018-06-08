1b15da732ae347c8114e1e0858f0aa2306ddacf9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:53:07 -0700

    CALLVALUE OpCode

---
+++ callvalue.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// CallValue represents the "CALLVALUE" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.CallValue{}.WriteTo(w)
+type CallValue struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (CallValue) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_CALLVALUE)
+}
