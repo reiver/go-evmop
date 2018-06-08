b7cd3ea7c0846dd3b4b0aaff9fd2f66dd276f38d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:57:06 -0700

    CALLDATACOPY OpCode

---
+++ calldatacopy.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// CallDataCopy represents the "CALLDATACOPY" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.CallDataCopy{}.WriteTo(w)
+type CallDataCopy struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (CallDataCopy) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_CALLDATACOPY)
+}
