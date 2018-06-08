b5f8450b03de4cc113a7b346783536b5fa4f95b3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 16:43:49 -0700

    created example for PUSH1

---
+++ example_push1_test.go
@@ -0,0 +1,29 @@
+package evmop_test
+
+import (
+	"github.com/reiver/go-evmop"
+
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ExamplePush1() {
+	var buffer bytes.Buffer
+
+	var writer io.Writer = &buffer
+
+	n, err := evmop.Push1{0x5}.WriteTo(writer)
+	if nil != err {
+		fmt.Fprintf(os.Stderr, "Problem writing “PUSH1 0x05” to buffer.\n")
+		return
+	}
+
+	fmt.Printf("Wrote %d bytes to buffer.\n", n)
+	fmt.Printf("Buffer: %#v\n", buffer.Bytes())
+
+	// Output:
+	// Wrote 2 bytes to buffer.
+	// Buffer: []byte{0x60, 0x5}
+}
