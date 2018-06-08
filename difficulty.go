bbd553add0d4056b20eb7e5f9f5f5bcb50c16e6f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 16:06:44 -0700

    DIFFICULTY OpCode

---
+++ difficulty.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Difficulty represents the "DIFFICULTY" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Difficulty{}.WriteTo(w)
+type Difficulty struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (Difficulty) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, CodeDifficulty)
+}
