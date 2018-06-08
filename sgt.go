250aba9901b08d8042f79d19d653958a14c3273f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:36:55 -0700

    SGT OpCode

---
+++ sgt.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// SGT represents the "SGT" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.SGT{}.WriteTo(w)
+type SGT struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (SGT) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_SGT)
+}
