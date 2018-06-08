36758da7739c5dc0a86b96cc25a9399c18e7c6ff
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 16:18:03 -0700

    PC OpCode

---
+++ pc.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// PC represents the "PC" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.PC{}.WriteTo(w)
+type PC struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (PC) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, CodePC)
+}
