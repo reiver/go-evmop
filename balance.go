6f834557ce749c97e62bc4239e5649bd8bf02ae4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:50:30 -0700

    BALANCE OpCode

---
+++ balance.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Balance represents the "BALANCE" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Balance{}.WriteTo(w)
+type Balance struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (Balance) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_BALANCE)
+}
