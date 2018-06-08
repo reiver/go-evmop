2619a0164bf5bd17747f748a5cd83cd62fb58e35
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:40:49 -0700

    OR OpCode

---
+++ or.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Or represents the "OR" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Or{}.WriteTo(w)
+type Or struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (Or) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_OR)
+}
