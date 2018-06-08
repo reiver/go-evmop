e29d654e480bc5b6b7e74bf951a5672027a3cc90
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:51:31 -0700

    ORIGIN OpCode

---
+++ origin.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Origin represents the "ORIGIN" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Origin{}.WriteTo(w)
+type Origin struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (Origin) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_ORIGIN)
+}
