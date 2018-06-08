ac9a54aa17e49e989db965c72608f79dfdee16e5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 15:34:50 -0700

    CODESIZE OpCode

---
+++ codesize.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// CodeSize represents the "CODESIZE" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.CodeSize{}.WriteTo(w)
+type CodeSize struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (CodeSize) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_CODESIZE)
+}
