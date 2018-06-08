ffd29259168397d688f0ea12841b3fea2136fd90
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:28:01 -0700

    SMOD OpCode

---
+++ smod.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// SMod represents the "SMOD" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.SMod{}.WriteTo(w)
+type SMod struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (SMod) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_SMOD)
+}
