15a36e5b9d5e42410fadef018d635e52707a4817
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:34:59 -0700

    GT OpCode

---
+++ gt.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// GT represents the "GT" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.GT{}.WriteTo(w)
+type GT struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (GT) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_GT)
+}
