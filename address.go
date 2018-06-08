43266b2a4e29be71305f5f304f319386eb8cfdb1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:49:51 -0700

    ADDRESS OpCode

---
+++ address.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Address represents the "ADDRESS" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Address{}.WriteTo(w)
+type Address struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (Address) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_ADDRESS)
+}
