b13df743759890c0dcb8b9a117361b0a49bf24ca
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 16:34:43 -0700

    PUSH2 OpCode

---
+++ push2.go
@@ -0,0 +1,27 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Push2 represents the "PUSH2" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Push2{ 0x01, 0x02 }.WriteTo(w)
+type Push2 struct {
+	Byte1 byte
+	Byte2 byte
+}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (receiver Push2) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, CodePush2, receiver.Byte1, receiver.Byte2)
+}
