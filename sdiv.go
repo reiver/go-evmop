0489080101939370fda2ce5406a78192b113f1b0
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:25:46 -0700

    SDIV OpCode

---
+++ sdiv.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// SDiv represents the "SDIV" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.SDiv{}.WriteTo(w)
+type SDiv struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (SDiv) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_SDIV)
+}
