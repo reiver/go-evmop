dfa21b8c7422ac8b8eb1abddc26598447c9deec2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:47:26 -0700

    SHA3 OpCode

---
+++ sha3.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// SHA3 represents the "SHA3" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.SHA3{}.WriteTo(w)
+type SHA3 struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (SHA3) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_SHA3)
+}
