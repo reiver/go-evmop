284fbc2de5c5511fa08482a3c9632c99a1804641
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 16:30:59 -0700

    made it so that the (unexported) func writeTo() access a variable length number of "more bytes" to write.

--- writeto.go
+++ writeto.go
@@ -4,7 +4,7 @@ import (
 	"io"
 )
 
-func writeTo(writer io.Writer, opcode byte) (int64, error) {
+func writeTo(writer io.Writer, opcode byte, moreBytes ...byte) (int64, error) {
 
 	if nil == writer {
 		return 0, errNilWriter
@@ -15,6 +15,19 @@ func writeTo(writer io.Writer, opcode byte) (int64, error) {
 	{
 		var b [1]byte = [1]byte{opcode}
 		p := b[:]
+
+		n, err := writer.Write(p)
+		if nil != err {
+			return n64, err
+		}
+		if 0 > n {
+			return n64, errNegativeNumberOfBytesWritten
+		}
+		n64 += int64(n)
+	}
+
+	if 0 < len(moreBytes) {
+		p := moreBytes
 
 		n, err := writer.Write(p)
 		if nil != err {
