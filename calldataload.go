b57ea4fb77615b6cdd486a75cf2e879857b87c1b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 13:55:20 -0700

    CALLDATALOAD OpCode

---
+++ calldataload.go
@@ -0,0 +1,24 @@
+package evmop
+
+import (
+	"io"
+)
+
+// CallDataLoad represents the "CALLDATALOAD" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.CallDataLoad{}.WriteTo(w)
+type CallDataLoad struct {}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (CallDataLoad) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, code_CALLDATALOAD)
+}
