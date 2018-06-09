c00bef9711f827c43f7dca6721edefcb770d002b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 21:03:27 -0700

    created example for PUSH12

---
+++ push12.go
@@ -0,0 +1,50 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Push12 represents the "PUSH11" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Push12{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c }.WriteTo(w)
+type Push12 struct {
+	Byte1 byte
+	Byte2 byte
+	Byte3 byte
+	Byte4 byte
+	Byte5 byte
+	Byte6 byte
+	Byte7 byte
+	Byte8 byte
+	Byte9 byte
+	Byte10 byte
+	Byte11 byte
+	Byte12 byte
+}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (receiver Push12) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, CodePush12,
+		receiver.Byte1,
+		receiver.Byte2,
+		receiver.Byte3,
+		receiver.Byte4,
+		receiver.Byte5,
+		receiver.Byte6,
+		receiver.Byte7,
+		receiver.Byte8,
+		receiver.Byte9,
+		receiver.Byte10,
+		receiver.Byte11,
+		receiver.Byte12,
+	)
+}
