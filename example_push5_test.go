a6d1c3fa7bc5999a4f8395f7f57182a69d6288b7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 17:01:23 -0700

    created example for PUSH5

---
+++ example_push5_test.go
@@ -0,0 +1,29 @@
+package evmop_test
+
+import (
+	"github.com/reiver/go-evmop"
+
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ExamplePush5() {
+	var buffer bytes.Buffer
+
+	var writer io.Writer = &buffer
+
+	n, err := evmop.Push5{0x05, 0x07, 0x09, 0x0b, 0x0d}.WriteTo(writer)
+	if nil != err {
+		fmt.Fprintf(os.Stderr, "Problem writing “PUSH2 0x05 0x07 0x09 0x0b” to buffer.\n")
+		return
+	}
+
+	fmt.Printf("Wrote %d bytes to buffer.\n", n)
+	fmt.Printf("Buffer: %#v\n", buffer.Bytes())
+
+	// Output:
+	// Wrote 6 bytes to buffer.
+	// Buffer: []byte{0x64, 0x5, 0x7, 0x9, 0xb, 0xd}
+}
---
+++ push5.go
@@ -0,0 +1,36 @@
+package evmop
+
+import (
+	"io"
+)
+
+// Push5 represents the "PUSH5" OpCode in the Ethereum Virtual Machine (EVM).
+//
+// Example Usage:
+//
+//	var w io.Writer
+//	
+//	// ...
+//	
+//	n, err := evmop.Push5{ 0x01, 0x02, 0x03 }.WriteTo(w)
+type Push5 struct {
+	Byte1 byte
+	Byte2 byte
+	Byte3 byte
+	Byte4 byte
+	Byte5 byte
+}
+
+// WriteTo writers the bytecodes for this Ethereum Virtual Machine OpCode to a io.Writer.
+//
+// WriteTo makes this struct fit the io.WriterTo interface.
+func (receiver Push5) WriteTo(writer io.Writer) (int64, error) {
+
+	return writeTo(writer, CodePush5,
+		receiver.Byte1,
+		receiver.Byte2,
+		receiver.Byte3,
+		receiver.Byte4,
+		receiver.Byte5,
+	)
+}
