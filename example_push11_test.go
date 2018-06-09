2417d40dd828247a113bde38de90fb4214d7070c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-06-08 21:00:07 -0700

    created example for PUSH11

---
+++ example_push11_test.go
@@ -0,0 +1,29 @@
+package evmop_test
+
+import (
+	"github.com/reiver/go-evmop"
+
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ExamplePush11() {
+	var buffer bytes.Buffer
+
+	var writer io.Writer = &buffer
+
+	n, err := evmop.Push11{0x05, 0x07, 0x09, 0x0b, 0x0d, 0x0f, 0x11, 0x13, 0x15, 0x17, 0x19}.WriteTo(writer)
+	if nil != err {
+		fmt.Fprintf(os.Stderr, "Problem writing “PUSH11 0x05 0x07 0x09 0x0b 0x0d 0x0f 0x11 0x13 0x15 0x17 0x19” to buffer.\n")
+		return
+	}
+
+	fmt.Printf("Wrote %d bytes to buffer.\n", n)
+	fmt.Printf("Buffer: %#v\n", buffer.Bytes())
+
+	// Output:
+	// Wrote 12 bytes to buffer.
+	// Buffer: []byte{0x6a, 0x5, 0x7, 0x9, 0xb, 0xd, 0xf, 0x11, 0x13, 0x15, 0x17, 0x19}
+}
